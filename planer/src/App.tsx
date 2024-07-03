a4e58ad16d6547c74cfa96bb3ccb522e27581ead
juliana <145568533+jvl1ana@users.noreply.github.com>
2024-07-03 08:20:11 +0200

    fixes routing issue

--- planer/src/App.tsx
+++ planer/src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
+import PersonalPage from './pages/personal/PersonalPage';
+import WorkPage from './pages/work/WorkPage';
+import SchoolPage from './pages/school/SchoolPage';
 
 
 function App() {
@@ -9,7 +12,11 @@ function App() {
     <>
       <Routes>
         <Route path="*" element={<Navigate replace to ="home"/>}/>
+        <Route path="/" element={<HomePage />} />
         <Route path="home" element={<HomePage/> } />
+        <Route path="school" element={<SchoolPage/> } />
+        <Route path="work" element={<WorkPage/> } />
+        <Route path="personal" element={<PersonalPage/> } />
       </Routes>
     </>
   )
--- planer/vite.config.ts
+++ planer/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/planer/',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/planer/' : '/'
+  };
 });

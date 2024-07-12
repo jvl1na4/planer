387579e7900cc6e76807c6d5ed10091890bfd483
juliana <145568533+jvl1ana@users.noreply.github.com>
2024-07-12 16:10:35 +0200

    wether & time field ! messy layout ha ha

--- planer/src/pages/home/HomePage.tsx
+++ planer/src/pages/home/HomePage.tsx
@@ -2,25 +2,35 @@ import { Box, Button, Grid } from "@mui/material";
 import Appbar from "../AppBar";
 import '/src/App.css';
 import Weather from "./WeatherCard";
-import CurrentDateWidget from "./GreatingCard";
+import CurrentDateWidget from "./DateCard";
 
 export default function HomePage() {
     
     return (
         <>
-            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', pt: 15 }}>
-                <Grid container spacing={2}>
+            <Box sx={{  flexGrow: 1, display: 'flex', flexDirection: 'column', pt: 15, }}>
+                <Grid container spacing={2} >
                     <Grid item xs={12}>
                         <Appbar/>
                     </Grid>
                     <Grid item sm={6}>  
-                         <Weather />
+                         <CurrentDateWidget />
                     </Grid>
                     <Grid item sm={6}>   
                         <CurrentDateWidget />            
+                    </Grid>
+                    <Grid item sm={4}>  
+                         <Weather />
+                    </Grid>
+                    <Grid item sm={4}>  
+                         <Weather />
+                    </Grid>
+                    <Grid item sm={4}>  
+                         <Weather />
                     </Grid>
                 </Grid>
             </Box>
         </>
     );
 }
+
